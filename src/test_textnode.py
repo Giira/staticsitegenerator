020c4d3eedf1419ca8cc71c2e21376210c128770
Giira <euan.a.marsh@gmail.com>
2024-09-24 14:37:39 +0100

    Class HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,29 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
 
+    def test_eq_false(self):
+        node = Textnode("This", "bold")
+        node2 = Textnode("this", "bold")
+        self.assertNotEqual(node, node2)
+
+    
+    def test_eq_false2(self):
+        node = Textnode("This", "bold")
+        node2 = Textnode("This", "italic")
+        self.assertNotEqual(node, node2)
+
+
+    def test_eq_url(self):
+        node = Textnode("This is a text node", "bold", "http://localhost:8888")
+        node2 = Textnode("This is a text node", "bold", "http://localhost:8888")
+        self.assertEqual(node, node2)
+
+    
+    def test_repr(self):
+        node = Textnode("This is a text node", "bold", "http://localhost:8888")
+        self.assertEqual(f"TextNode(This is a text node, bold, http://localhost:8888)", repr(node))
+
+
+
 if __name__ == "__main__":
     unittest.main()
-    
