c3168919ce335b7bc3b442de51715bca11b02339
Giira <euan.a.marsh@gmail.com>
2024-09-24 11:40:54 +0100

    Added first unit test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from textnode import Textnode
 
 
 def main():
-    text_node = Textnode("text", "bold", "https://localhost:8888")
+    text_node = Textnode("text", "bold", "http://localhost:8888")
 
 
 main()
