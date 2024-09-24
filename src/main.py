08b0559ab61c0b93976ec7f2b8f51acb43e23402
Giira <euan.a.marsh@gmail.com>
2024-09-24 11:02:31 +0100

    Initial setup

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import Textnode
+
+
+def main():
+    text_node = Textnode("text", "bold", "https://localhost:8888")
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,16 @@
+class Textnode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    
+    def __eq__(self, Textnode):
+        if self.text == Textnode.text and self.text_type == Textnode.text_type and self.url == Textnode.url:
+            return True
+        else:
+            return False
+        
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
