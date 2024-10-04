fdfc9b536489c34beb4857d86bf5ff7dc1f360e4
Giira <euan.a.marsh@gmail.com>
2024-10-04 11:50:20 +0100

    consolidate HTMLNode children and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,4 +23,45 @@ class HTMLNode():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Invalid HTML: no value")
+        elif self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        html_string = ""
+        for node in self.children:
+            html_string += node.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
     
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq_props(self):
@@ -15,3 +15,53 @@ class TestHTMLNode(unittest.TestCase):
     def test_eq_None(self):
         node = HTMLNode(tag="h2")
         self.assertEqual(None, node.value)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode(value="Print here", tag="a", props={"href": "https://www.google.com"})
+        self.assertEqual('<a href="https://www.google.com">Print here</a>', node.to_html())
+
+    def test_no_tag(self):
+        node = LeafNode(None, "Hello world!")
+        self.assertEqual(node.to_html(), "Hello world!")
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_eq(self):
+        node = ParentNode("p", [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ], )
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
+    def test_th_props_eq(self):
+        node = ParentNode("p", [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ], 
+        {"href": "www.google.com", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<p href="www.google.com" target="_blank"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>')
+
+    def test_leaf_props(self):
+        node = ParentNode("p", [
+            LeafNode("b", "Bold text", {"href": "www.google.com"}),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ], )
+        self.assertEqual(node.to_html(), '<p><b href="www.google.com">Bold text</b>Normal text<i>italic text</i>Normal text</p>')
+
+    def test_single_child(self):
+        node = ParentNode("h2", [LeafNode("b", "child")])
+        self.assertEqual(node.to_html(), "<h2><b>child</b></h2>")
+
+    def test_grandchild(self):
+        grandnode = LeafNode("span", "grandchild")
+        childnode = ParentNode("i", [grandnode])
+        node = ParentNode("h3", [childnode])
+        self.assertEqual(node.to_html(), '<h3><i><span>grandchild</span></i></h3>')
