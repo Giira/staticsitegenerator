76cc76081d901af9c840750d37c51a1630fb7a3f
Giira <euan.a.marsh@gmail.com>
2024-10-15 15:59:06 +0100

    block to block type func

--- src/markdownblocks.py
+++ src/markdownblocks.py
@@ -5,3 +5,41 @@ def markdown_to_blocks(markdown):
         if item != "":
             output_list.append(item)
     return output_list
+
+
+def block_to_block_type(block):
+    line_list = block.split("\n")
+    if block[0] == "#":
+        hash_counter = 0
+        for letter in block:
+            if letter == "#":
+                hash_counter += 1
+                if hash_counter > 6:
+                    return "paragraph"
+                else:
+                    continue
+            if letter != " ":
+                return "paragraph"
+            elif letter == " ":
+                return "heading"
+    elif block[0:3] == "```" and len(block) >6 and block[-3:] == "```":
+        return "code"
+    elif block[0] == ">":
+        for line in line_list:
+            if line[0] != ">":
+                return "paragraph"
+        return "quote"
+    elif block[0:2] == "* " or block[0:2] == "- ":
+        for line in line_list:
+            if line[:2] != "* " and line[:2] != "- ":
+                return "paragraph"
+        return "unordered_list"
+    elif block[0].isnumeric() and block[1:3] == ". ":
+        counter = 1
+        for line in line_list:
+            if int(line[0]) != counter or line[1:3] != ". ":
+                return "paragraph"
+            counter += 1
+        return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_markdownblocks.py
+++ src/test_markdownblocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdownblocks import markdown_to_blocks
+from markdownblocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_eq(self):
@@ -33,3 +33,69 @@ Paragraph
             "Paragraph",
             "*List 1\n*List 2\n*List 3"
         ])
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_eq_heading(self):
+        block = "###### block"
+        self.assertEqual(block_to_block_type(block), "heading")
+
+    def test_eq_heading2(self):
+        block = "####### block"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
+    def test_eq_heading3(self):
+        block = "###block"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
+    def test_eq_code(self):
+        block = "```code```"
+        self.assertEqual(block_to_block_type(block), "code")
+
+    def test_eq_code2(self):
+        block = "```code``"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+    
+    def test_eq_code_3(self):
+        block = "``````"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+    
+    def test_eq_code4(self):
+        block = "``` ```"
+        self.assertEqual(block_to_block_type(block), "code")
+
+    def test_eq_quote(self):
+        block = ">line\n>line2\n>line3"
+        self.assertEqual(block_to_block_type(block), "quote")
+
+    def test_eq_quote2(self):
+        block = ">line\nline2"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
+    def test_one_quote(self):
+        block = ">quote"
+        self.assertEqual(block_to_block_type(block), "quote")
+    
+    def test_eq_unordered_list(self):
+        block = "* 1\n* 2\n* 3"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+
+    def test_eq_unordered_list2(self):
+        block = "* 1\n- 2\n* 3"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+    
+    def test_eq_unordered_list3(self):
+        block = "* 1\n*2\n* 3"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
+    def test_eq_ordered_list(self):
+        block = "1. line\n2. line\n3. line"
+        self.assertEqual(block_to_block_type(block), "ordered_list")
+
+    def test_eq_ordered_list2(self):
+        block = "1. line\n4. line\n3. line"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+
+    def test_eq_ordered_list2(self):
+        block = "1. line\n2. line\n3.line"
+        self.assertEqual(block_to_block_type(block), "paragraph")
